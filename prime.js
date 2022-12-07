8ecfb39bfce64bb46cd49e06276ae8617964f7d7
Abhiraj19 <abhiabhiraj784@gmail.com>
2022-12-07 10:10:25 +0530

    task is compleated

--- prime.js
+++ prime.js
@@ -1,10 +1,13 @@
-let a= 13;
+   let a= 13;
    let count=0;
 for(let i=0; i<=a; i++){
-if(a%2==0){
+if(a%i==0){
 count++
 }
 }
-if(count==2)
+if(count==2){
+  console.log(a,"is prime")
+}else{
+  console.log(a,"is not prime")
 }
-console.log(count)
+
